e88505fb1a802d524fb5e278299d40a7184d4e13
Laura M <112851761+LauraMoonJam@users.noreply.github.com>
2022-09-19 11:02:44 +0300

    Add files via upload

---
+++ DARBINSmasivi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+    int main(){
+
+         int x []={1, 5, 9 ,69, 420};
+
+        cout<<sizeof(x)/sizeof(int)<<endl;
+
+
+
+/*1katrs simbols ieskaitot atstarpi un komatu ir dala no koda katrs simbols ir atseviska kaste kuru var izmantot koda piemeram cout-ot
+konkretu simbolu izmantojot skaitli
+piem(cout<<x[skaitlis]<<endl;)
+2masivi-string(simboliem un vardiem) un int(skaitliem) piem;
+ka skaitit cik ir masiva garums ( int x []={1, 5, 9 ,69, 420};
+    cout<<sizeof(x)/sizeof(int)<<endl;)
+
+*/
+    }
---
+++ darbsstundamasivi(neiznaca).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+  int main() {
+   int x[3]={8, 6, 9};
+
+   cout<<"lielskais skaitlis"<<endl;
+   if (0>2); {
+    cout <<0;
+   }
+   if (0>2) {
+    cout <<}
+   if ()
+
+  }
