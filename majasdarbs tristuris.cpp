bf031d507b19de83c578fd86ac15b6218f532f3a
Laura M <112851761+LauraMoonJam@users.noreply.github.com>
2022-09-20 20:29:25 +0300

    Add files via upload

---
+++ majasdarbs tristuris.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+    int main (){
+
+        double a;
+        double b;
+        double c;
+
+    cout<<"ievadi pirmas malas garumu"<<endl;
+    cin>>a;
+    cout<<"ievadi otras malas garumu"<<endl;
+    cin>>b;
+    cout<<"ievadi tresas malas garumu"<<endl;
+    cin>>c;
+
+
+
+
+
+    if (a+b<c, a+c<b, b+c<a){
+        cout<<"nav tristuris"<<endl;
+        }
+    else{
+        cout<<"ir tristuris"<<endl;
+    }
+
+
+
+
+    }
+
+
