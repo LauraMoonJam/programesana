ef7c95d454560edf7e9857ef23ab1201ab779064
Laura M <112851761+LauraMoonJam@users.noreply.github.com>
2022-10-03 11:01:52 +0300

    Add files via upload

---
+++ faktorialimajasd.cpp
@@ -0,0 +1,71 @@
+/*faktoriali ir skaitlis! pirm 5! kas ir 5*4*3*2*1
+
+#include <iostream>
+using namespace std;
+
+
+int main (){
+
+  int x = 1;
+  int y;
+
+
+  cout<<"ievadiet skaitli"<<endl;
+  cin>>y;
+
+  for(y;y >= 1; y--){
+        x = x*y;
+  }
+    cout<<x;
+
+}
+
+#include <iostream>
+using namespace std;
+
+
+int main (){
+    int m2 = 1, m1 = 1, rez = 0;
+    int sk;
+
+    cin>>sk;
+
+    for(int i =1; i<=sk; i++){
+        rez = m1+m2;
+    cout<<rez<<"";
+    m2 = m1;
+    m1 = rez;
+}
+
+
+}  */
+
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+int sk;
+int i=1;
+
+cout<<"ievadi skaitli"<<endl;
+cin>>sk;
+
+
+
+
+
+
+vajag uztaisit kodu lai butu tristuris mazakais lidz ievaditaiis skaitlis
+piem. n=4
+*
+**
+***
+****
+
+
+
+}
